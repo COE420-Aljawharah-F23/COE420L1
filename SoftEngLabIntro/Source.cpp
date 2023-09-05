bfde60071145b333274c7cc531c0db465c6c597c
AnasHakmi <anashakmi25@gmail.com>
2023-09-05 18:25:01 +0400

    Initial Commit

--- SoftEngLabIntro/Source.cpp
+++ SoftEngLabIntro/Source.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+using namespace std;
+
+void featureB() {
+    cout << "COE420 Lab 1" << endl;
+}
+
 
 int main()
 {
     std::cout << "Hello World!\n";
+    featureB();
 }
+
 
